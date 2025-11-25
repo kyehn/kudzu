0956f7de8484882b66562631b0ff5679e08a1112
kyehn <kyehn@users.noreply.github.com>
2025-11-25 08:24:42 +0800

    first commit

---
+++ home-manager.nix
@@ -0,0 +1,297 @@
+{
+  inputs,
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+
+{
+  imports = [ inputs.nix-index-database.homeModules.nix-index ];
+
+  home = {
+    stateVersion = lib.trivial.release;
+    language.base = "zh_CN.UTF-8";
+  };
+
+  xdg = {
+    configFile = {
+      "nixpkgs/config.nix".text = ''
+        {
+          allowUnfree = true;
+          android_sdk.accept_license = true;
+        }
+      '';
+      "pip/pip.conf".text = ''
+        [global]
+        index-url = https://mirror.nju.edu.cn/pypi/web/simple
+      '';
+    };
+  };
+
+  programs = {
+    man.generateCaches = false;
+    nix-index = {
+      enable = true;
+      enableBashIntegration = false;
+      enableFishIntegration = false;
+    };
+    atuin = {
+      enable = true;
+      flags = [ "--disable-up-arrow" ];
+      settings = {
+        auto_sync = false;
+        update_check = false;
+        show_help = false;
+        enter_accept = true;
+        prefers_reduced_motion = true;
+      };
+    };
+    ripgrep = {
+      enable = true;
+      arguments = [ "--ignore-case" ];
+    };
+    bun = {
+      enable = false;
+      settings = {
+        smol = true;
+        telemetry = false;
+        install.registry = "https://npmreg.proxy.ustclug.org";
+        run.bun = true;
+      };
+    };
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        set -g fish_greeting
+        set -U fish_history_max 10000
+        set -gx fish_prompt_pwd_dir_length 0
+        fish_config theme choose 'Tomorrow Night Bright'
+        fish_config prompt choose simple
+      ''
+      + lib.optionalString config.programs.bun.enable ''
+        set -x BUN_INSTALL $HOME/.bun
+        set -x PATH $PATH $BUN_INSTALL/bin
+      '';
+      shellAbbrs = {
+        nix-wipe = "nix profile wipe-history --older-than 1d && ${lib.getExe pkgs.home-manager} expire-generations 0days";
+        nix-gc = "nix-collect-garbage --delete-older-than 7d";
+      };
+    };
+    broot = {
+      enable = true;
+      settings.default_flags = "-ih";
+    };
+    htop = {
+      enable = true;
+      settings = {
+        fields = [
+          0
+          48
+          20
+          49
+          39
+          40
+          111
+          46
+          47
+          1
+        ];
+        hide_userland_threads = 1;
+        show_thread_names = 1;
+        show_program_path = 0;
+        highlight_base_name = 1;
+        strip_exe_from_cmdline = 1;
+        show_merged_command = 0;
+        screen_tabs = 1;
+        cpu_count_from_one = 1;
+        show_cpu_frequency = 1;
+        show_cpu_temperature = 1;
+        color_scheme = 6;
+        column_meters_0 = [
+          "CPU"
+          "Memory"
+          "Swap"
+        ];
+        column_meter_modes_0 = [
+          1
+          1
+          1
+        ];
+        column_meters_1 = [
+          "Tasks"
+          "DiskIO"
+          "NetworkIO"
+        ];
+        column_meter_modes_1 = [
+          2
+          2
+          2
+        ];
+        tree_view = 0;
+        sort_key = 47;
+        sort_direction = -1;
+      };
+    };
+    yazi = {
+      enable = true;
+      settings = {
+        opener.edit = [
+          {
+            run = "$\{EDITOR:-hx\} \"$@\"";
+            block = true;
+            for = "unix";
+          }
+        ];
+        mgr = {
+          show_hidden = true;
+          sort_dir_first = true;
+        };
+        preview.wrap = "yes";
+      };
+    };
+    bat = {
+      enable = true;
+      config = {
+        style = "header-filename,header-filesize,grid";
+        paging = "never";
+        theme = "Dracula";
+      };
+    };
+    fd = {
+      enable = true;
+      hidden = true;
+      ignores = [
+        ".git/"
+        # Dependency directories
+        "node_modules/"
+        # Caches
+        ".ipynb_checkpoints/"
+        ".cache/"
+        ".next/"
+        # Python
+        ".venv/"
+        "__pycache__/"
+      ];
+      extraOptions = [
+        "--no-ignore-vcs"
+        "--full-path"
+      ];
+    };
+    eza = {
+      enable = true;
+      icons = "auto";
+      git = true;
+      colors = "auto";
+      extraOptions = [
+        "--group-directories-first"
+        "--all"
+      ];
+    };
+    helix = {
+      enable = true;
+      defaultEditor = true;
+      settings = {
+        theme = "fleet_dark";
+        editor = {
+          middle-click-paste = false;
+          file-picker.hidden = false;
+          soft-wrap.enable = true;
+          indent-guides.render = true;
+        };
+      };
+    };
+    git = {
+      enable = true;
+      lfs.skipSmudge = true;
+      ignores = [
+        # Environments
+        ".env"
+        ".env.local"
+        ".env.*.local"
+        # Dependency directories
+        "node_modules/"
+        # IDEs and Editors
+        ## JetBrains IDEs
+        ".idea/"
+        # Logs and runtime files
+        "*.log"
+        "*.seed"
+        "*.temp"
+        ".cmp"
+        ".ipynb_checkpoints/"
+        ".cache/"
+        ".next/"
+        # Operating System
+        ".DS_Store"
+        # Node
+        ".npm"
+        ".eslintcache"
+        ".stylelintcache"
+        # Python
+        ".venv/"
+        ".Python"
+        "*.py[cod]"
+        "__pycache__/"
+        "pyvenv.cfg"
+        "pip-selfcheck.json"
+        # CMake
+        "CMakeFiles/"
+        "CMakeScripts/"
+        "CMakeCache.txt"
+        "*.cmake"
+        # Maven
+        "pom.xml.tag"
+        "pom.xml.releaseBackup"
+        "pom.xml.versionsBackup"
+        # Databases
+        "*.db"
+        "*.sqlite3-journal"
+      ];
+      attributes = [ "*.age diff=nodiff" ];
+      settings = {
+        user = {
+          name = "jane";
+          email = "jane@computer.local";
+        };
+        core = {
+          autocrlf = "input";
+          askpass = "";
+          quotepath = false;
+        };
+        init.defaultBranch = "main";
+        push.autoSetupRemote = true;
+        difftool.prompt = false;
+        diff.nodiff.command = "true";
+        log.date = "iso";
+        merge.conflictStyle = "zdiff3";
+        alias = {
+          ca = "commit --amend --no-edit --reset-author --no-date";
+          pf = "push --force";
+        };
+      };
+      signing.format = "openpgp";
+    };
+    delta = {
+      enable = true;
+      options.side-by-side = false;
+      enableGitIntegration = true;
+    };
+    uv = {
+      enable = true;
+      settings.pip.index-url = "https://mirror.nju.edu.cn/pypi/web/simple";
+    };
+    ruff = {
+      enable = true;
+      settings = {
+        preview = true;
+        unsafe-fixes = true;
+        format.docstring-code-format = true;
+      };
+    };
+  };
+
+  manual.manpages.enable = false;
+
+  news.display = "silent";
+}
---
+++ nix-on-droid.nix
@@ -0,0 +1,70 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  environment = {
+    packages = with pkgs; [
+      typst
+      age
+      rfv
+      riffdiff
+      gnused
+      comma
+      (python313.withPackages (
+        ps: with ps; [
+          requests
+          python-dotenv
+        ]
+      ))
+      (pkgs.writeShellScriptBin "patchedpython" ''
+        export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+        exec python $@
+      '')
+    ];
+    motd = null;
+    etcBackupExtension = ".bak";
+    sessionVariables = {
+      EDITOR = "hx";
+      LESS = "-SR";
+      MANPAGER = "sh -c 'col -bx | ${lib.getExe pkgs.bat} -l man -p'";
+      MANROFFOPT = "-c";
+    };
+  };
+
+  nix = {
+    package = pkgs.nix;
+    substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://nix-community.cachix.org"
+      "https://cache.garnix.io"
+      "https://seilunako.cachix.org"
+    ];
+    trustedPublicKeys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+      "seilunako.cachix.org-1:e/aJJI1S5hPY/BPeiVZcuPjt5ZjBRRo9dlYHmvwXPFM="
+    ];
+    nixPath = [ "nixpkgs=${pkgs.path}" ];
+    extraOptions = ''
+      always-allow-substitutes = true
+      auto-allocate-uids = true
+      experimental-features = nix-command flakes auto-allocate-uids
+      eval-cores = 0
+      lazy-trees = true
+      min-free = 10240
+      require-sigs = false
+      show-trace = true
+      warn-dirty = false       
+    '';
+  };
+
+  user.shell = lib.getExe pkgs.fish;
+
+  system.stateVersion = "25.11";
+
+  time.timeZone = "Etc/GMT-8";
+}
