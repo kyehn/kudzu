a35f68b8a74bc4df276aa6a0cd585e1450385575
kyehn <kyehn@users.noreply.github.com>
2026-01-17 17:18:42 +0800

    first commit

---
+++ overlays/default.nix
@@ -0,0 +1,16 @@
+{ inputs, ... }:
+
+final: prev: {
+  uutils-tar = prev.callPackage ./uutils-tar.nix { };
+  nix = inputs.nix.packages."${prev.stdenv.hostPlatform.system}".default;
+  rfv = prev.writeShellScriptBin "rfv" (
+    builtins.readFile (
+      prev.replaceVars ./rfv {
+        rg = prev.lib.getExe prev.ripgrep;
+        fzf = prev.lib.getExe prev.fzf;
+        hx = prev.lib.getExe prev.helix;
+        bat = prev.lib.getExe prev.bat;
+      }
+    )
+  );
+}
