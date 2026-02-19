c5d6896ab335e4314011da8beb3fdc21da7c4422
kyehn <228304369+kyehn@users.noreply.github.com>
2026-02-20 06:01:44 +0800

    first commit

---
+++ nix-on-droid.nix
@@ -0,0 +1,93 @@
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  environment = {
+    packages = with pkgs; [
+      age
+      bashInteractive
+      comma
+      curl
+      uutils-diffutils
+      gawk
+      gnugrep
+      gnupatch
+      gnused
+      util-linux
+      gzip
+      less
+      uutils-coreutils-noprefix
+      uutils-findutils
+      procps
+      ncurses
+      netcat-openbsd
+      openssh
+      opencode
+      rfv
+      riffdiff
+      time
+      which
+      (python314.withPackages (
+        ps: with ps; [
+          requests
+          python-dotenv
+        ]
+      ))
+      (pkgs.writeShellScriptBin "patchedpython" ''
+        export LD_LIBRARY_PATH=$NIX_LD_LIBRARY_PATH
+        exec python $@
+      '')
+    ];
+    etcBackupExtension = ".bak";
+    sessionVariables = {
+      EDITOR = "hx";
+      LESS = "-SR";
+      MANPAGER = "sh -c '${lib.getExe' pkgs.util-linux "col"} -bx | ${lib.getExe pkgs.bat} -l man -p'";
+      MANROFFOPT = "-c";
+    };
+  };
+
+  nix = {
+    package = pkgs.nix;
+    extraOptions = lib.mkForce ''
+      sandbox = false
+      allow-unsafe-native-code-during-evaluation = true
+      always-allow-substitutes = true
+      builders-use-substitutes = true
+      experimental-features = nix-command flakes
+      trusted-users =
+      lazy-trees = true
+      show-trace = true
+      warn-dirty = false
+      flake-registry = ${
+        pkgs.writeText "flake-registry.json" (
+          builtins.toJSON {
+            version = 2;
+            flakes = map (name: {
+              from = {
+                id = name;
+                type = "indirect";
+              };
+              to = {
+                type = "path";
+                path = inputs.${name};
+              };
+            }) (builtins.filter (name: name != "self") (builtins.attrNames inputs));
+          }
+        )
+      }
+      nix-path = nixpkgs=${inputs.nixpkgs}
+      substituters = https://nix-community.cachix.org https://cache.nixos.org https://cache.garnix.io https://seilunako.cachix.org
+      trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g= seilunako.cachix.org-1:e/aJJI1S5hPY/BPeiVZcuPjt5ZjBRRo9dlYHmvwXPFM=
+      system-features = nixos-test benchmark
+    '';
+  };
+
+  user.shell = lib.getExe pkgs.fish;
+
+  time.timeZone = "Etc/GMT-8";
+}
---
+++ overlays/default.nix
@@ -0,0 +1,26 @@
+{ inputs, ... }:
+
+final: prev: {
+  nix = inputs.nix.packages."${prev.stdenv.hostPlatform.system}".default;
+  rfv = prev.writeShellScriptBin "rfv" (
+    builtins.readFile (
+      prev.replaceVars ./rfv {
+        rg = prev.lib.getExe prev.ripgrep;
+        fzf = prev.lib.getExe prev.fzf;
+        hx = prev.lib.getExe prev.helix;
+        bat = prev.lib.getExe prev.bat;
+      }
+    )
+  );
+  opencode = prev.opencode.overrideAttrs (oldAttrs: {
+    installPhase =
+      prev.lib.replaceStrings
+        [
+          "wrapProgram $out/bin/opencode"
+        ]
+        [
+          "wrapProgram $out/bin/opencode --set OPENCODE_DISABLE_DEFAULT_PLUGINS true --set OPENCODE_DISABLE_LSP_DOWNLOAD true"
+        ]
+        oldAttrs.installPhase;
+  });
+}
